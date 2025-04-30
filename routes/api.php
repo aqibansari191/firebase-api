9d20c7399e15ec13444c20e9c69c9ff7cff34aff
abhishekkumar6207 <abhishekkumar6207351661@gmail.com>
2025-04-30 15:42:28 +0530

    new
    new

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\MembershipController;
 use App\Http\Controllers\userController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrdersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,13 +18,12 @@ Route::post('/login', [userController::class, 'login']);
 Route::post('/membership', [MembershipController::class, 'store']);
 Route::get('/membership/{id}', [MembershipController::class, 'show']);
 Route::get('/allmembership', [MembershipController::class, 'index']);
+
 Route::post('/groups', [HomeController::class, 'store']);
 Route::get('/groups', [HomeController::class, 'getAllData']);
-
 Route::get('/groups/{id}', [HomeController::class, 'getSingleData']);
 
 Route::post('/orders', [OrdersController::class, 'insertOrder']);
-
 Route::get('/GetAllOrders', [OrdersController::class, 'getAllOrders']);
 Route::get('/getSingleOrder/{id}', [OrdersController::class, 'getSingleOrder']);
 
