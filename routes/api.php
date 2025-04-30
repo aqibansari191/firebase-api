b8524a7a792c3468664611db6f8c5423b25e9f9b
abhishekkumar6207 <abhishekkumar6207351661@gmail.com>
2025-04-30 11:09:37 +0530

    hello
    helo

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,13 @@
 
 use App\Http\Controllers\MembershipController;
 use App\Http\Controllers\userController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 
 Route::post('signup', [userController::class, 'authentication']);
@@ -16,3 +17,15 @@ Route::post('/login', [userController::class, 'login']);
 Route::post('/membership', [MembershipController::class, 'store']);
 Route::get('/membership/{id}', [MembershipController::class, 'show']);
 Route::get('/allmembership', [MembershipController::class, 'index']);
+Route::post('/groups', [HomeController::class, 'store']);
+Route::get('/groups', [HomeController::class, 'getAllData']);
+
+Route::get('/groups/{id}', [HomeController::class, 'getSingleData']);
+
+Route::post('/orders', [OrdersController::class, 'insertOrder']);
+
+Route::get('/GetAllOrders', [OrdersController::class, 'getAllOrders']);
+Route::get('/getSingleOrder/{id}', [OrdersController::class, 'getSingleOrder']);
+
+
+
